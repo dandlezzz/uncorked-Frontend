d99888db2eedac3f47c762361303d7074c87d495
Dandlezzz <danieladammiller@gmail.com>
2014-05-25 21:20:12 -0700

    minor adjustments, started to work on client side win conditions

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -11,6 +11,7 @@ angular.module('uncorkedApp').controller('GameController', function($http, $root
 		}).success(function(data){
 			$rootScope.currentUser.state = data;
 			self.displayGame(data)
+
 		})
 	};
 	this.bet = {};
@@ -18,15 +19,21 @@ angular.module('uncorkedApp').controller('GameController', function($http, $root
 	this.dealerCards = []; 
 	this.playerCards = [];
 	this.playerHandValue =0;
+	this.houseHandValue = 0;
+	this.winConditions = {
+		player: false,
+		push: false
+	}
 	
 
 	this.placeBet = function(bet){
 		this.playerCards = [];
 		this.dealerCards = [];
+		this.houseHandValue = 0;
 		var self = this;
 		$http({
 			url : "http://localhost:3000/api/tables/1",
-			params : {"bet":this.bet.amount, "token" : $rootScope.currentUser.auth_token.access_token},
+			params : {"bet": this.bet.amount, "token" : $rootScope.currentUser.auth_token.access_token},
 			method: "PUT",
 			headers : {'Content-Type': 'application/json'}
 		}).success(function(data){
@@ -45,13 +52,19 @@ angular.module('uncorkedApp').controller('GameController', function($http, $root
 		var self = this;
 		this.playerHandValue = 0;
 		data['User Hand Value'].forEach(function(val){
-				console.log(val)
-
 				self.playerHandValue += val;
 			})
 	};
-	this.hit= function(){
+	this.updateHouseHandValue = function(data){
+		var self= this;
+		this.houseHandValue = 0
+		data['House Hand Value'].forEach(function(val){
+			self.houseHandValue += val;
+		})
+	}
+	this.hit = function(){
 		var self = this;
+		this.playerCards =[]
 		$http({
 			url : "http://localhost:3000/api/tables/1",
 			params : {"decision":'hit', "token" : $rootScope.currentUser.auth_token.access_token},
@@ -60,7 +73,9 @@ angular.module('uncorkedApp').controller('GameController', function($http, $root
 		}).success(function(data){
 			$rootScope.currentUser.state = data
 			console.log(data)
-			self.playerCards.push(Cards.makeImagePath(data.Hand[data.Hand.length-1]))
+			data.Hand.forEach(function(card){
+				self.playerCards.push(Cards.makeImagePath(card))
+			})
 			self.updatePlayerHandValue(data)
 		})
 	};
@@ -79,6 +94,8 @@ angular.module('uncorkedApp').controller('GameController', function($http, $root
 				self.dealerCards.push(Cards.makeImagePath(card))
 			});
 			console.log(data)
+			self.updateHouseHandValue(data)
+
 		});
 	};
 
@@ -88,6 +105,15 @@ angular.module('uncorkedApp').controller('GameController', function($http, $root
 		}
 		return true;
 	}
+	// this.calculateWin = function(){
+	// 	if (this.playerHandValue === 21 && this.houseHandValue !== 21){
+	// 		return this.winConditions.player = true;
+	// 	} else if (this.playerHandValue > 21){
+	// 		return false;
+	// 	} else if (this.playerHandValue < 21 && this.playerHandValue > this.houseHandValue){
+	// 		return this.winConditions.player = true;
+	// 	}
+	// }
 });
 
 //1 is ace 10 is 10 11 is jack 13 is king
